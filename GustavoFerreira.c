6bdd7b1e0403d7cca7fdfde43d8abee50a85be8d
guupenna <110266747+guupenna@users.noreply.github.com>
2022-08-08 11:12:11 -0300

    Update GustavoFerreira.c

--- GustavoFerreira.c
+++ GustavoFerreira.c
@@ -14,6 +14,16 @@ typedef struct {
     char posicoes[TAM_MAX][TAM_MAX];
 } tMapa;
 
+// Estatisticas
+typedef struct {
+    int movSemPonto;
+    int movBaixo;
+    int movCima;
+    int movEsquerda;
+    int movDireita;
+    int movTotal;
+} tEstatistica;
+
 // Cobra
 #define COBRA_DIREITA '>'
 #define COBRA_ESQUERDA '<'
@@ -29,6 +39,7 @@ typedef struct {
     int tamanho;
     int cresceuAgr;
     char direcao;
+    tEstatistica estatisticas;
 } tCobra;
 
 // Jogo
@@ -57,18 +68,18 @@ typedef struct {
     int numero;
 } tJogada;      // Utilizado para mapear e montar histórico de jogadas
 
-void RealizaJogo(tJogo jogo, FILE *fsaida);
+void RealizaJogo(tJogo jogo, FILE *fsaida, FILE *fresumo, FILE *franking, FILE *festatisticas, FILE *fheatmap);
 
-tJogo ExecutaJogada(tJogo jogo, tJogada jogada, tPosicao pos[]);
+tJogo ExecutaJogada(tJogo jogo, tJogada jogada, tPosicao pos[], FILE *fresumo);
 
-tJogo AtualizaJogo(tJogo jogo, tJogada jogada, tPosicao pos[]);
+tJogo AtualizaJogo(tJogo jogo, tJogada jogada, tPosicao pos[], FILE *fresumo);
 
 void ImprimeSaida(tJogo jogo, tJogada jogada, FILE *fsaida);
 
 // Movimento e ações com a cobra
-void MarcaPosAnteriorCabeca(tCobra cobra, tJogada jogada, tPosicao pos[]);
+void MarcaPosicaoCabeca(tCobra cobra, tJogada jogada, tPosicao pos[]);
 
-tCobra MoveCabeca(tCobra cobra, tJogada jogada, tPosicao pos[]);
+tCobra MoveCabeca(tCobra cobra, tEstatistica estatisticas, tJogada jogada, tPosicao pos[]);
 
 tCobra MantemCobraNoMapa(tCobra cobra, tMapa mapa);
 
@@ -80,8 +91,23 @@ tCobra CresceCobra(tCobra cobra, tJogada jogada, tPosicao pos[]);
 
 tCobra MataCobra(tCobra cobra, tMapa mapa);
 
-// Auxiliares
+// Resumo
+void ImprimeNoResumo(FILE *fresumo, tJogada jogada, tCobra cobra, int acao);
 
+// Heatmap
+void ImprimeHeatmap(int l, int c, int heatmap[l][c], tJogada jogada, tPosicao pos[], FILE *fheatmap);
+
+// Ranking
+void ImprimeRanking(int l, int c, int heatmap[l][c], FILE *franking);
+
+// Estatisticas
+tEstatistica ObtemEstatisticas(tCobra cobra);
+
+tCobra AdicionaMovSemPontoEstatistica (tCobra cobra, tEstatistica estatistica);
+
+void ImprimeEstatisticas(tCobra cobra, FILE *festatisticas);
+
+// Auxiliares
 tPosicao ObtemPosicaoCabeca(tCobra cobra);
 
 int ObtemTamanhoLinhas(tMapa mapa);
@@ -89,6 +115,10 @@ int ObtemTamanhoLinhas(tMapa mapa);
 int ObtemTamanhoColunas(tMapa mapa);
 
 tJogo PoeDentroDetJogo(tMapa mapa, tCobra cobra, tJogo jogo);
+
+tCobra InicializaCorpo(tCobra cobra);
+
+tCobra ZeraEstatisticas(tCobra cobra);
 
 int CresceuAgr(tCobra cobra);
 
@@ -115,12 +145,12 @@ char RetornaMapaNaPosicao(tMapa mapa, int i, int j);
 int EhVitoria(tMapa mapa);
 
 int main(int argc, char * argv[]) {
-    FILE *fileMapa, *fileInicializacao, *fileSaida;
+    FILE *fileMapa, *fileInicializacao, *fileResumo, *fileRanking, *fileEstatisticas, *fileHeatmap, *fileSaida;
     char diretorio[1000];
     tJogo jogo;
 
     // Verifica se foi inserido o nome do diretório
-    if (argv[1]==NULL) {
+    if (argv[1] == NULL) {
         printf("ERRO: O diretorio de arquivos de configuracao nao foi informado\n");
         return 0;
     }
@@ -140,14 +170,31 @@ int main(int argc, char * argv[]) {
 
     jogo = InicializaJogo(jogo, fileMapa, fileInicializacao);
 
-    //
+    //Abrir todos os arquivos a serem utilizados para a realização do jogo
     sprintf(diretorio, "%s/saida/saida.txt", argv[1]);
     fileSaida = fopen(diretorio, "w");
 
-    RealizaJogo(jogo, fileSaida);
+    sprintf(diretorio, "%s/saida/resumo.txt", argv[1]);
+    fileResumo = fopen(diretorio, "w");
 
+    sprintf(diretorio, "%s/saida/ranking.txt", argv[1]);
+    fileRanking = fopen(diretorio, "w");
+
+    sprintf(diretorio, "%s/saida/estatisticas.txt", argv[1]);
+    fileEstatisticas = fopen(diretorio, "w");
+
+    sprintf(diretorio, "%s/saida/heatmap.txt", argv[1]);
+    fileHeatmap = fopen(diretorio, "w");
+
+    RealizaJogo(jogo, fileSaida, fileResumo, fileRanking, fileEstatisticas, fileHeatmap);
+
+    // Fecha todos os arquivos abertos anteriormente
     fclose(fileMapa);
     fclose(fileInicializacao);
+    fclose(fileResumo);
+    fclose(fileRanking);
+    fclose(fileEstatisticas);
+    fclose(fileHeatmap);
     fclose(fileSaida);
 
     return 0;
@@ -188,6 +235,8 @@ tPosicao ObtemPosicaoCabeca(tCobra cobra) {
 
     posicao.i = cobra.lcabeca;
     posicao.j = cobra.ccabeca;
+
+    return posicao;
 }
 
 tMapa LeTamanhoMapa(tMapa mapa, FILE *fmapa) {
@@ -240,43 +289,79 @@ tJogo PoeDentroDetJogo(tMapa mapa, tCobra cobra, tJogo jogo) {
     return jogo;
 }
 
-void RealizaJogo(tJogo jogo, FILE *fsaida) {
+void RealizaJogo(tJogo jogo, FILE *fsaida, FILE *fresumo, FILE *franking, FILE *festatisticas, FILE *fheatmap) {
     tJogada jogada;
-    tPosicao pos[1000] = {'\0'};
-    jogada.numero = 0;
+    tPosicao pos[TAM_MAX*TAM_MAX] = {'\0'};
+    jogada.numero = 1;
     jogo.pts = 0;
     jogo.gameOver = 0;
+    jogo.cobra = InicializaCorpo(jogo.cobra);
+    jogo.cobra = ZeraEstatisticas(jogo.cobra);
 
     while (scanf("%c", &jogada.movimento) == 1) {
         scanf("%*c");
         
-        jogo = ExecutaJogada(jogo, jogada, pos);
+        jogo = ExecutaJogada(jogo, jogada, pos, fresumo);
 
         ImprimeSaida(jogo, jogada, fsaida);
 
+        jogada.numero++;
+
         if (EhGameOver(jogo)) {
-            printf("Game over!\n");
+            fprintf(fsaida, "Game over!\n");
             break;
         }
         if (EhVitoria(jogo.mapa)) {
-            printf("Voce venceu!\n");
+            fprintf(fsaida, "Voce venceu!\n");
             break;
         }
-
-        jogada.numero++;
     }
-    printf("Pontuacao final: %d\n", jogo.pts);
+    fprintf(fsaida, "Pontuacao final: %d\n", jogo.pts);
+
+    MarcaPosicaoCabeca(jogo.cobra, jogada, pos);    // Para marcar a posição final da cabeça, que fez o jogo terminar
+
+    int l, c;
+
+    l = ObtemTamanhoLinhas(jogo.mapa);
+    c = ObtemTamanhoColunas(jogo.mapa);
+
+    int heatmap[l][c];
+
+    ImprimeHeatmap(l, c, heatmap, jogada, pos, fheatmap);
+    ImprimeRanking(l, c, heatmap, franking);
+
+    ImprimeEstatisticas(jogo.cobra, festatisticas);
 }
 
-tJogo ExecutaJogada(tJogo jogo, tJogada jogada, tPosicao pos[]) { 
-    // Marcar posicao da cabeca antes do movimento, para realizar a troca de posicoes sequencial para movimentar o corpo
-    MarcaPosAnteriorCabeca(jogo.cobra, jogada, pos);
+tCobra InicializaCorpo(tCobra cobra) {
+    int i, j;
 
-    jogo.cobra = MoveCabeca(jogo.cobra, jogada, pos);
+    for (i=0; i<TAM_MAX; i++) {
+        for (j=0; j<TAM_MAX; j++) {
+            cobra.corpo[i][j] = '\0';
+        }
+    }
+
+    return cobra;
+}
+
+tCobra ZeraEstatisticas(tCobra cobra) {
+    tEstatistica estatistica = {0};
+    cobra.estatisticas = estatistica;
+
+    return cobra;
+}
+
+tJogo ExecutaJogada(tJogo jogo, tJogada jogada, tPosicao pos[], FILE *fresumo) { 
+    // Marcar posicao da cabeca antes do movimento, para realizar a troca de posicoes sequencial para movimentar o corpo
+    MarcaPosicaoCabeca(jogo.cobra, jogada, pos);
+
+    tEstatistica estatistica = ObtemEstatisticas(jogo.cobra);
+    jogo.cobra = MoveCabeca(jogo.cobra, estatistica, jogada, pos);
 
     jogo.cobra = MantemCobraNoMapa(jogo.cobra, jogo.mapa);
 
-    jogo = AtualizaJogo(jogo, jogada, pos);
+    jogo = AtualizaJogo(jogo, jogada, pos, fresumo);
 
     // Primeiro movimenta a cabeça para identificar e realizar ação necessária, e depois movimenta o corpo
     if (!CresceuAgr(jogo.cobra)) {
@@ -290,6 +375,10 @@ tJogo ExecutaJogada(tJogo jogo, tJogada jogada, tPosicao pos[]) {
     return jogo;
 }
 
+tEstatistica ObtemEstatisticas(tCobra cobra) {
+    return cobra.estatisticas;
+}
+
 int CresceuAgr(tCobra cobra) {
     return cobra.cresceuAgr;
 }
@@ -298,77 +387,91 @@ int EhGameOver(tJogo jogo) {
     return jogo.gameOver;
 }
 
-void MarcaPosAnteriorCabeca(tCobra cobra, tJogada jogada, tPosicao pos[]) {
-    pos[jogada.numero].i=cobra.lcabeca;
-    pos[jogada.numero].j=cobra.ccabeca;
+void MarcaPosicaoCabeca(tCobra cobra, tJogada jogada, tPosicao pos[]) {
+    pos[jogada.numero].i = cobra.lcabeca;
+    pos[jogada.numero].j = cobra.ccabeca;
 }
 
-tCobra MoveCabeca(tCobra cobra, tJogada jogada, tPosicao pos[]) {
+tCobra MoveCabeca(tCobra cobra, tEstatistica estatisticas, tJogada jogada, tPosicao pos[]) {
     if (cobra.direcao == COBRA_DIREITA) {
         switch(jogada.movimento) {
             case 'c':
             cobra.ccabeca++;
+            estatisticas.movDireita++;
             break;
 
             case 'h':
             cobra.lcabeca++;
             cobra.direcao = COBRA_BAIXO;
+            estatisticas.movBaixo++;
             break;
 
             case 'a':
             cobra.lcabeca--;
             cobra.direcao = COBRA_CIMA;
+            estatisticas.movCima++;
             break;
         }
     } else if (cobra.direcao == COBRA_ESQUERDA) {
         switch(jogada.movimento) {
             case 'c':
             cobra.ccabeca--;
+            estatisticas.movEsquerda++;
             break;
 
             case 'h':
             cobra.lcabeca--;
             cobra.direcao = COBRA_CIMA;
+            estatisticas.movCima++;
             break;
 
             case 'a':
             cobra.lcabeca++;
             cobra.direcao = COBRA_BAIXO;
+            estatisticas.movBaixo++;
             break;
         }
     } else if (cobra.direcao == COBRA_BAIXO) {
         switch(jogada.movimento) {
             case 'c':
             cobra.lcabeca++;
+            estatisticas.movBaixo++;
             break;
 
             case 'h':
             cobra.ccabeca--;
             cobra.direcao = COBRA_ESQUERDA;
+            estatisticas.movEsquerda++;
             break;
 
             case 'a':
             cobra.ccabeca++;
             cobra.direcao = COBRA_DIREITA;
+            estatisticas.movDireita++;
             break;
         }
     } else if (cobra.direcao == COBRA_CIMA) {
         switch(jogada.movimento) {
             case 'c':
             cobra.lcabeca--;
+            estatisticas.movCima++;
             break;
 
             case 'h':
             cobra.ccabeca++;
             cobra.direcao = COBRA_DIREITA;
+            estatisticas.movDireita++;
             break;
 
             case 'a':
             cobra.ccabeca--;
             cobra.direcao = COBRA_ESQUERDA;
+            estatisticas.movEsquerda++;
             break;
         }
     }
+
+    cobra.estatisticas = estatisticas;
 
     return cobra;
 }
@@ -390,13 +493,15 @@ tCobra MantemCobraNoMapa(tCobra cobra, tMapa mapa) {
     return cobra;
 }
 
-tJogo AtualizaJogo(tJogo jogo, tJogada jogada, tPosicao pos[]) {
+tJogo AtualizaJogo(tJogo jogo, tJogada jogada, tPosicao pos[], FILE *fresumo) {
     int i, j, l, c, temAcao;
 
     l = ObtemTamanhoLinhas(jogo.mapa);
     c = ObtemTamanhoColunas(jogo.mapa);
 
     jogo.cobra = ResetaCresceuAgr(jogo.cobra);
+    
+    tEstatistica estatistica = ObtemEstatisticas(jogo.cobra);
 
     for (i=0; i<l; i++) {
         for (j=0; j<c; j++) {
@@ -406,27 +511,69 @@ tJogo AtualizaJogo(tJogo jogo, tJogada jogada, tPosicao pos[]) {
                     char mapaNaPos = RetornaMapaNaPosicao(jogo.mapa, i, j);
                     temAcao = IdentificaAcao(mapaNaPos);
 
-                    if (temAcao == 1) {
-                        jogo.cobra = CresceCobra(jogo.cobra, jogada, pos);
-                        jogo.pts++;
-                    }
-                    if (temAcao == 2) {
-                        jogo.pts+=10;
+                    if (!temAcao) {
+                        jogo.cobra = AdicionaMovSemPontoEstatistica(jogo.cobra, estatistica);
                     }
                     if (temAcao == -1) {
                         jogo.gameOver = 1;
+                        jogo.cobra = AdicionaMovSemPontoEstatistica(jogo.cobra, estatistica);
                     }
+                    if (temAcao == 1) {
+                        jogo.pts += 10;
+                    }
+                    if (temAcao == 2) {
+                        jogo.pts++;
+                        jogo.cobra = CresceCobra(jogo.cobra, jogada, pos);
+                    }
+                    // Substitui a posição no mapa que a cabeça da cobra passou, por um espaço vazio
                     char vazio = ' ';
                     jogo.mapa = PoeNoMapaNaPosicao(jogo.mapa, i, j, vazio);
-                } else if (!EhUltimaParteCorpo(jogo.cobra, jogada, pos)) // Verifica se a cabeca não está na posicao da ultima parte do corpo. Caso esteja, a cobra não morre, já que o corpo ainda será movido e aquela parte será liberada.
+
+                    if (EhVitoria(jogo.mapa)) {
+                        temAcao = 3;
+                    }
+
+                } else {
+                    jogo.cobra = AdicionaMovSemPontoEstatistica(jogo.cobra, estatistica);
+                    if (!EhUltimaParteCorpo(jogo.cobra, jogada, pos)) // Verifica se a cabeca não está na posicao da ultima parte do corpo. Caso esteja, a cobra não morre, já que o corpo ainda será movido e aquela parte será liberada.
                     {
                         jogo.gameOver = 1;
+                        temAcao = -1;
                     }
+                }
+                ImprimeNoResumo(fresumo, jogada, jogo.cobra, temAcao);
                 return jogo;
             }
         }
     }
     return jogo;
+}
+
+tCobra AdicionaMovSemPontoEstatistica (tCobra cobra, tEstatistica estatistica) {
+    estatistica.movSemPonto++;
+    cobra.estatisticas = estatistica;
+
+    return cobra;
+}
+
+void ImprimeNoResumo(FILE *fresumo, tJogada jogada, tCobra cobra, int acao) {
+    switch (acao) {
+        case -1:
+        fprintf(fresumo, "Movimento %d (%c) resultou no fim de jogo por conta de colisao\n", jogada.numero, jogada.movimento);
+        break;
+
+        case 1:
+        fprintf(fresumo, "Movimento %d (%c) gerou dinheiro\n", jogada.numero, jogada.movimento);
+        break;
+
+        case 2:
+        fprintf(fresumo, "Movimento %d (%c) fez a cobra crescer para o tamanho %d\n", jogada.numero, jogada.movimento, cobra.tamanho);
+        break;
+
+        case 3:
+        fprintf(fresumo, "Movimento %d (%c) fez a cobra crescer para o tamanho %d, terminando o jogo\n", jogada.numero, jogada.movimento, cobra.tamanho);
+        break;
+    }
 }
 
 tCobra ResetaCresceuAgr(tCobra cobra) {
@@ -460,12 +607,12 @@ int EhPosicaoCorpo(tCobra cobra, int i, int j) {
 }
 
 int IdentificaAcao(char sign) {
-    if (sign == MAP_COMIDA) {
-        return 1;
-    } else if (sign == MAP_DINHEIRO) {
-        return 2;
-    } else if (sign == MAP_PAREDE || sign == COBRA_CORPO) {
+    if (sign == MAP_PAREDE) {
         return -1;
+    } else if (sign == MAP_DINHEIRO) {
+        return 1;
+    } else if (sign == MAP_COMIDA) {
+        return 2;
     } else {
         return 0;
     }
@@ -515,25 +662,25 @@ void ImprimeSaida(tJogo jogo, tJogada jogada, FILE *fsaida) {
     l = ObtemTamanhoLinhas(jogo.mapa);
     c = ObtemTamanhoColunas(jogo.mapa);
 
-    printf("\nEstado do jogo apos o movimento '%c':\n", jogada.movimento);
+    fprintf(fsaida, "\nEstado do jogo apos o movimento '%c':\n", jogada.movimento);
     for (i=0; i<l; i++) {
         for (j=0; j<c; j++) {
             if (EhPosicaoCorpo(jogo.cobra, i, j)) {
-                int corpoNaPosicao = RetornaCorpoNaPosicao(jogo.cobra, i, j);
-                printf("%c", corpoNaPosicao);
+                char corpoNaPosicao = RetornaCorpoNaPosicao(jogo.cobra, i, j);
+                fprintf(fsaida, "%c", corpoNaPosicao);
                 continue;
             }
             if (EhPosicaoCabeca(jogo.cobra, i, j)) {
                 char cabeca = ObtemCabeca(jogo.cobra);
-                printf("%c", cabeca);
+                fprintf(fsaida, "%c", cabeca);
                 continue;
             }
             char mapaNaPosicao = RetornaMapaNaPosicao(jogo.mapa, i, j);
-            printf("%c", mapaNaPosicao);
+            fprintf(fsaida, "%c", mapaNaPosicao);
         }
-        printf("\n");
+        fprintf(fsaida, "\n");
     }
-    printf("Pontuacao: %d\n", jogo.pts);
+    fprintf(fsaida, "Pontuacao: %d\n", jogo.pts);
 }
 
 char ObtemCabeca(tCobra cobra) {
@@ -582,4 +729,63 @@ tCobra MataCobra(tCobra cobra, tMapa mapa) {
     }
 
     return cobra;
+}
+
+void ImprimeHeatmap(int l, int c, int heatmap[l][c], tJogada jogada, tPosicao pos[], FILE *fheatmap) {
+    int i, j, k = 1;
+
+    // Preenche o heatmap com '0'
+    for (i=0; i<l; i++) {
+        for (j=0; j<c; j++) {
+            heatmap[i][j] = 0;
+        }
+    }
+
+    // Verifica todas as posicoes que a cabeca passou em jogadas anteriores e atribui ao heatmap com a frequencia correspondente
+    while ( k<=jogada.numero ) {
+        i = pos[k].i;
+        j = pos[k].j;
+        heatmap[i][j]++;
+        k++;
+    }
+
+    for (i=0; i<l; i++) {
+        for (j=0; j<c; j++) {
+            fprintf(fheatmap, "%d", heatmap[i][j]);
+            if (j == c-1) {
+                fprintf(fheatmap, "\n");
+            } else {
+                fprintf(fheatmap, " "); 
+            }
+        }
+    }
+}
+
+void ImprimeRanking(int l, int c, int heatmap[l][c], FILE *franking) {
+    int maiorValor = 0, i, j, n;
+
+    // Marca o maior valor para ter um ponto de partida na verificação das posições mais frequentes
+    for (i=0; i<l; i++) {
+        for (j=0; j<c; j++) {
+            if (heatmap[i][j] > maiorValor) {
+                maiorValor = heatmap[i][j];
+            }
+        }
+    }
+    for (n=maiorValor; n>=1; n--)
+        for (i=0; i<l; i++) {
+            for (j=0; j<c; j++) {
+                if (heatmap[i][j] == n) {
+                    fprintf(franking, "(%d, %d) - %d\n", i, j, n);
+                }
+            }
+        }
+}
+
+void ImprimeEstatisticas(tCobra cobra, FILE *festatisticas) {
+    tEstatistica estatistica = ObtemEstatisticas(cobra);
+
+    estatistica.movTotal = estatistica.movBaixo + estatistica.movCima + estatistica.movDireita + estatistica.movEsquerda;
+
+    fprintf(festatisticas, "Numero de movimentos: %d\nNumero de movimentos sem pontuar: %d\nNumero de movimentos para baixo: %d\nNumero de movimentos para cima: %d\nNumero de movimentos para esquerda: %d\nNumero de movimentos para direita: %d\n", estatistica.movTotal, estatistica.movSemPonto, estatistica.movBaixo, estatistica.movCima, estatistica.movEsquerda, estatistica.movDireita);
 }
